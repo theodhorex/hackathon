e0163bf73757bf2de2579a6c39fa7a6436fd4a4c
Kevin Glory Prasetyo 71220958 <117893068+Kevin-Glory-Prasetyo@users.noreply.github.com>
2025-11-20 21:02:42 +0700

    kevin extensionV2

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import ExtensionPanel from "./components/ExtensionPanel";
+import ExtensionPanel_1 from "./components/ExtensionPanel_1";
 
 export default function Home() {
   return (
     <div>
-      <ExtensionPanel />
+      <ExtensionPanel_1 />
       <h1>Hlelloooo</h1>
       <h1>sdifhsdhfjsdfhjsdfs</h1>
 
