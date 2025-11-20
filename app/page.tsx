a6fa065456099cbcd8554b209fafa8ce41cc1665
theodhorex <theodhore76@gmail.com>
2025-11-20 21:38:46 +0700

    test theo

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,7 @@ import ExtensionPanel_1 from "./components/ExtensionPanel_1";
 export default function Home() {
   return (
     <div>
-      <h1>Hello</h1>
-
+      <h1>oke</h1>
     </div>
   );
 }
