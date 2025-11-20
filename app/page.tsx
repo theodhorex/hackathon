4cbbae1147ad509dd27ee63f4d239d105a8ec298
alventendrawan123 <143707973+alventendrawan123@users.noreply.github.com>
2025-11-20 20:28:14 +0700

    oke

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,12 @@ import ExtensionPanel from "./components/ExtensionPanel";
 
 export default function Home() {
   return (
+<<<<<<< Updated upstream
     <div>
       <ExtensionPanel />
     </div>
+=======
+    <h1 className="text-xs">helo</h1>
+>>>>>>> Stashed changes
   );
 }
