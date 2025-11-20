e04011f1a964694c53ac8b8f62913ae4857c8d43
theodhorex <theodhore76@gmail.com>
2025-11-20 21:26:04 +0700

    memindahkan page awal ke page extension

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,7 @@ import ExtensionPanel_1 from "./components/ExtensionPanel_1";
 export default function Home() {
   return (
     <div>
-      <ExtensionPanel_1 />
-      <h1>Hlelloooo</h1>
-      <h1>sdifhsdhfjsdfhjsdfs</h1>
-      <h1>pp</h1>
-      <h1>ppsdfghjuytredcvghjuytredfghjuytredcvbnhfdfghjhgfdedfghjhgfeddfghjhgfdefgh</h1>
+      <h1>Hello</h1>
 
     </div>
   );
