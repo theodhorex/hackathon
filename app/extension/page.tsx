d15890b75a99c627bc4c925dc15c1e3a635f157d
alventendrawan123 <143707973+alventendrawan123@users.noreply.github.com>
2025-11-20 22:15:49 +0700

    push1

--- app/extension/page.tsx
+++ app/extension/page.tsx
@@ -1,11 +1,12 @@
 import ExtensionPanel from "../components/ExtensionPanel";
 import ExtensionPanel_1 from "../components/ExtensionPanel_1";
 import Extension_Panel_joe from "../components/Extension_Panel_joe";
+import Extension_Panel_alven from "../components/Extension_Panel_alven";
 
 export default function Extension() {
   return (
     <div>
-      <Extension_Panel_joe />
+      <Extension_Panel_alven />
     </div>
   );
 }
